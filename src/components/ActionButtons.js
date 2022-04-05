82a61858f0449baed72742394e39a57db4189bd2
Branislav Brincko <branobrincko@gmail.com>
2022-04-05 23:07:44 +0200

    Hiding back button on 1st step, new css utility classes
    - when on first step, back button is not visible
    - implemented using new css utility classes
    - classnames library installed and used

--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -1,24 +1,38 @@
+import classNames from "classnames";
 import React from "react";
 import { NUMBER_OF_STEPS } from "../constants";
 import { isFormStepValid } from "../helpers/formValidation";
 
 function ActionButtons({ currentStepId, setCurrentStepId }) {
-  // Helpers and handlers
-  const isFormStep = (stepId) => stepId === currentStepId;
+  // Functions
   const goToPrevStep = () => {
     if (currentStepId > 0) setCurrentStepId(currentStepId - 1);
   };
+
   const goToNextStep = () => {
     if (currentStepId < NUMBER_OF_STEPS - 1) setCurrentStepId(currentStepId + 1);
   };
 
+  const isFormStep = (stepId) => stepId === currentStepId;
+
+  // Values
   const canContinue = (isFormStep(0) && isFormStepValid(0)) || (isFormStep(1) && isFormStepValid(1));
   const canSubmit = isFormStep(2) && isFormStepValid(2);
 
+  // Classes
+  const actionButtonsContainerClasses = classNames("action-buttons-container", {
+    "justify-end": isFormStep(0),
+    "justify-between": !isFormStep(0),
+  });
+
+  const actionButtonBackClasses = classNames("action-button", "action-button-back", {
+    "d-hidden": isFormStep(0),
+  });
+
   // Component
   return (
-    <div className="action-buttons-container">
-      <button className="action-button action-button-back" type="button" onClick={goToPrevStep}>
+    <div className={actionButtonsContainerClasses}>
+      <button className={actionButtonBackClasses} type="button" onClick={goToPrevStep}>
         Späť
       </button>
       {isFormStep(2) ? (
