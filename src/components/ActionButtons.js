83d58ed3adee182b188c36bac103567f5dbb77c2
Branislav Brincko <branobrincko@gmail.com>
2022-04-07 10:26:33 +0200

    Correct form action buttons shown for steps
    - first step only next button
    - second step next and back button
    - third step submit and back button
    - fourth step only again button

--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -17,6 +17,10 @@ function ActionButtons({ currentStepId, setCurrentStepId }) {
     if (currentStepId < NUMBER_OF_STEPS - 1) setCurrentStepId(currentStepId + 1);
   };
 
+  const goToFirstStep = () => {
+    setCurrentStepId(0);
+  };
+
   const submitForm = (e) => {
     e.preventDefault();
     dispatch(createContribution());
@@ -32,28 +36,54 @@ function ActionButtons({ currentStepId, setCurrentStepId }) {
   // Classes
   const actionButtonsContainerClasses = classNames("action-buttons-container", {
     "justify-end": isFormStep(0),
-    "justify-between": !isFormStep(0),
+    "justify-between": !isFormStep(0) && !isFormStep(3),
+    "justify-center": isFormStep(3),
   });
-
   const actionButtonBackClasses = classNames("action-button", "action-button-back", {
     "d-hidden": isFormStep(0),
   });
 
+  const ButtonBack = () => (
+    <button className={actionButtonBackClasses} type="button" onClick={goToPrevStep}>
+      Späť
+    </button>
+  );
+
+  const ButtonAgain = () => (
+    <button className="action-button action-button-next" type="button" onClick={goToFirstStep}>
+      Vrátiť sa na začiatok
+    </button>
+  );
+
+  const ButtonSubmit = () => (
+    <button className="action-button action-button-next" type="submit" onClick={submitForm} disabled={!canSubmit}>
+      Odoslať formulár
+    </button>
+  );
+
+  const ButtonNext = () => (
+    <button className="action-button action-button-next" type="button" onClick={goToNextStep} disabled={!canContinue}>
+      Pokračovať
+    </button>
+  );
+
   // Component
   return (
     <div className={actionButtonsContainerClasses}>
-      <button className={actionButtonBackClasses} type="button" onClick={goToPrevStep}>
-        Späť
-      </button>
-      {isFormStep(2) ? (
-        <button className="action-button action-button-next" type="submit" onClick={submitForm} disabled={!canSubmit}>
-          Odoslať formulár
-        </button>
-      ) : (
-        <button className="action-button action-button-next" type="button" onClick={goToNextStep} disabled={!canContinue}>
-          Pokračovať
-        </button>
+      {isFormStep(0) && <ButtonNext />}
+      {isFormStep(1) && (
+        <>
+          <ButtonBack />
+          <ButtonNext />
+        </>
       )}
+      {isFormStep(2) && (
+        <>
+          <ButtonBack />
+          <ButtonSubmit />
+        </>
+      )}
+      {isFormStep(3) && <ButtonAgain />}
     </div>
   );
 }
