d04d057658d76470971b97321ff0e1affac02855
Branislav Brincko <branobrincko@gmail.com>
2022-04-07 21:40:37 +0200

    Classnames library used for generating classes

--- src/components/DoubleButton.js
+++ src/components/DoubleButton.js
@@ -2,28 +2,27 @@ import { ReactComponent as WalletIcon } from "../images/WalletIcon.svg";
 import { ReactComponent as PawIcon } from "../images/PawIcon.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { updateForm } from "../store";
+import classNames from "classnames";
 
 function DoubleButton() {
   const { useShelterID } = useSelector((state) => state.form);
   const dispatch = useDispatch();
-
-  const getActiveClass = (btnId) => {
-    if (btnId === 0) return useShelterID ? "active" : "";
-    if (btnId === 1) return !useShelterID ? "active" : "";
-  };
 
   const handleClick = (btnId) => {
     const payload = { useShelterID: btnId === 0 };
     dispatch(updateForm(payload));
   };
 
+  const classesButton1 = classNames("double-button", { active: useShelterID });
+  const classesButton2 = classNames("double-button", { active: !useShelterID });
+
   return (
     <div className="double-button-container">
-      <button type="button" className={"double-button " + getActiveClass(0)} onClick={() => handleClick(0)}>
+      <button type="button" className={classesButton1} onClick={() => handleClick(0)}>
         <WalletIcon className="double-button-icon" />
         <span>Chcem finančne prispieť konkrétnemu útulku</span>
       </button>
-      <button type="button" className={"double-button " + getActiveClass(1)} onClick={() => handleClick(1)}>
+      <button type="button" className={classesButton2} onClick={() => handleClick(1)}>
         <PawIcon className="double-button-icon" />
         <span>Chcem finančne prispieť celej nadácii</span>
       </button>
