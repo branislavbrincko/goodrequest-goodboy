bdc72216f9a5b366aa29c46761792d90ace7982b
Branislav Brincko <branobrincko@gmail.com>
2022-04-08 12:19:19 +0200

    Added transition to input error message
    - it slides down from the top

--- src/components/InputErrorMessage.js
+++ src/components/InputErrorMessage.js
@@ -1,9 +1,15 @@
+import classNames from "classnames";
 import React from "react";
 import { useSelector } from "react-redux";
 
 function InputErrorMessage({ fieldName }) {
   const errors = useSelector((state) => state.form.errors);
-  return <div className="input-error-message"> {errors[fieldName]} </div>;
+
+  const inputErrorMessageClasses = classNames("input-error-message", {
+    "input-error-message-active": errors[fieldName],
+  });
+
+  return <div className={inputErrorMessageClasses}> {errors[fieldName]} </div>;
 }
 
 export default InputErrorMessage;
